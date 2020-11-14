f05fb252f750e4d7aa0c4dfcc0a9c302a4a100cf
AntohiRobert <antohir@gmail.com>
2020-11-14 14:03:10 +0200

    Added BGI

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# Statically linked sdl2 library
+# Statically linked sdl2 library plus sdl_bgi
 Two examples of hello world-ish windows, sdl2 statically linked.  
 Uses latest stable release on unofficial github mirror for sdl2.  
+One example of bgi program(with Graphics.h)
 Tested on: 
  * Windows 10, CLion 2020.2.4
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
+#include <graphics.h>
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
@@ -81,10 +82,18 @@ int example2()
     return 0;
 }
 
+int example3()
+{
+    initwindow(800,600); int x = 400, y = 300, r;
+    for (r = 25; r <= 125; r += 20)
+        circle(x, y, r);
+    getch(); closegraph(); return 0;
+}
+
 //arguments for main?idk
 //int argc, char* args[]
 
 int main() {
-    example1();
+    example3();
     return 0;
 }
